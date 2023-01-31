241d6a9e9ff4cd18e1bb5a1b89b83c68152b785f
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2023-01-31 11:48:33 +0530

    index port changes

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ App.get("/", (req, res) => {
 App.use("/api/auth/", require("./Routes/auth_router"));
 App.use("/api/note/", require("./Routes/notes_router"));
 
-App.listen(serverConfig.PORT ? serverConfig.PORT : 8080, () => {
+App.listen(serverConfig.PORT ? serverConfig.PORT : 8082, () => {
   console.log(
     `Server is running on http://localhost:${
-      serverConfig.PORT ? serverConfig.PORT : 8080
+      serverConfig.PORT ? serverConfig.PORT : 8082
     }`
   );
 });
